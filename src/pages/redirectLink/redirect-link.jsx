1900bd45757da9c348bd9748df4695b83d505203
tranvanluc <davidchoi839@gmail.com>
2024-07-08 23:31:20 +0700

    update helmet npm

--- src/pages/redirectLink/redirect-link.jsx
+++ src/pages/redirectLink/redirect-link.jsx
@@ -1,16 +1,16 @@
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function RedirectLink() {
 
   return (
-    <div>
+    <HelmetProvider>
       <Helmet>
             <meta charSet="utf-8" />
             <meta name="title" content="ban hang online"></meta>
             <meta name="description" content="Free Web tutorials"></meta>
             <link rel="media" href="https://landkit.goodthemes.co/assets/img/illustrations/illustration-2.png" />
       </Helmet>
-    </div>
+    </HelmetProvider>
   );
 }
 
