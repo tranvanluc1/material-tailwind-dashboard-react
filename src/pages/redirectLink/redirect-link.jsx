cacaa86380c94660f4547912d2f07da3f6230050
tranvanluc <davidchoi839@gmail.com>
2024-07-09 23:03:49 +0700

    .

--- src/pages/redirectLink/redirect-link.jsx
+++ src/pages/redirectLink/redirect-link.jsx
@@ -3,15 +3,18 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 export function RedirectLink() {
 
   return (
-    <HelmetProvider>
+    <div>
+      <HelmetProvider>
       <Helmet>
             <meta charSet="utf-8" />
             <meta name="title" content="ban hang online"></meta>
             <meta name="description" content="Free Web tutorials"></meta>
             <link rel="media" href="https://landkit.goodthemes.co/assets/img/illustrations/illustration-2.png" />
       </Helmet>
-      1
-    </HelmetProvider>
+      </HelmetProvider>
+    1
+    </div>
+    
   );
 }
 
